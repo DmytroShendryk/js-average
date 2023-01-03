7f976fa7e43e6178bcadf99652644515790e1340
DmytroShendryk <dimoon674@gmail.com>
2023-01-03 21:15:52 +0200

    average-fixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 let numOne = +prompt ("enter your number");
 let numTwo = +prompt ("enter your number");
 let numThree = +prompt ("enter your number");
-const arr = [numOne, numTwo, numThree];
-let sum = 0;    
-arr.forEach((num) => { sum += num });
-average = sum / arr.length;
-alert(sum / arr.length);
+let average = ((numOne+numTwo+numThree)/3);
+alert(average)
 console.log(average);
